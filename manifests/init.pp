f1d679bd5fd085948302ea7e7f9c91b31703cf97
Thiago Ribeiro <ribeiro.it@gmail.com>
2017-02-02 00:38:52 -0200

    Fixing start command

--- manifests/init.pp
+++ manifests/init.pp
@@ -197,8 +197,9 @@ class boh(
     if $environment == 'dev' {
         exec {
             'boh-start':
-                command => "/bin/bash -c \"export DJANGO_SETTINGS_MODULE='project.settings.${environment}';${basename}env/bin/python${python_version} ${basename}project/manage.py runserver\"",
-                require => Exec['boh-compilemessages'];
+                environment => ["DJANGO_SETTINGS_MODULE='project.settings.${environment}'"],
+                command     => "${basename}project/manage.py runserver",
+                require     => Exec['boh-compilemessages'];
         }
     }
 }
